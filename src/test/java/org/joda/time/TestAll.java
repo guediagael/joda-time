d8021cd3318001ec403e2c79a8d8593fa8b69ed2
guediagael <e.guedia@innopolis.ru>
2017-04-26 22:30:52 +0300

    testjenkins first scenario

--- src/test/java/org/joda/time/TestAll.java
+++ src/test/java/org/joda/time/TestAll.java
@@ -134,7 +134,7 @@ public class TestAll extends TestCase {
     }
 
     public static void main(String args[]) {
-				System.out.println("Hello test 2");
+				System.out.println("First scenario");
 
         String[] testCaseName = {
             TestAll.class.getName()
